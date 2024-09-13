e810a54fdbc0bd0b011e5f00b7f908bc5eb39c79
Tyler Anatole <tanatol000@citymail.cuny.edu>
2024-09-13 18:52:18 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Tyler',
+    author_email='tanatol000@citymail.cuny.edu',
+    packages=find_packages(), 
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
